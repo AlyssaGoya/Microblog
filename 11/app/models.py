2fab9d2464f8cc1dbc008b150e1ef6a83d58636b
sad alyssa <104743863+AlyssaGoya@users.noreply.github.com>
2023-05-08 20:57:52 +0800

    Added fans and unit tests.
    Added fans and unit tests.
    新增了粉丝与单元测试功能。

---
+++ 11/app/models.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+from hashlib import md5
+
+
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow())
+
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        # return 'https://www.gravator.com/avatar/{}? d=identicon={}'.format(digest, size)
+        # return 'https://tse4-mm.cn.bing.net/th/id/OIP-C.nDJn6jO-bh7AzEOdcLiiCQAAAA?w=165&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7{}? d=identicon={}'.format(digest, size)
+        return 'https://www.cnavatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
+
